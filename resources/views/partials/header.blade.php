c881dc82d6df277f1ba1a6b8f482c6b8cdfb4d43
valeriofilareti <valerio.filareti@gmail.com>
2023-06-01 15:57:36 +0200

    [link] about page in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
 <header>
     <nav>
         <ul>
-            <li><a href="#">link</a></li>
-            <li><a href="#">link</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="about">About</a></li>
             <li><a href="#">link</a></li>
         </ul>
     </nav>
