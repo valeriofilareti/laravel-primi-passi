78959cc44fe6ab3c19145e9e95ea0cd94df525c8
valeriofilareti <valerio.filareti@gmail.com>
2023-06-01 16:00:12 +0200

    [add] new page + new route contacts

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @include('partials.header')
+<h1>Contacts</h1>
+<h3>contatto 1</h3>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos eveniet atque, veniam ex porro corrupti amet pariatur expedita. Dignissimos pariatur suscipit accusamus ea expedita officia quia deserunt magnam aspernatur, illum provident quibusdam tempora, autem commodi quas repellendus eveniet iure consequatur?</p>
+<h3>contatto 2</h3>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos eveniet atque, veniam ex porro corrupti amet pariatur expedita. Dignissimos pariatur suscipit accusamus ea expedita officia quia deserunt magnam aspernatur, illum provident quibusdam tempora, autem commodi quas repellendus eveniet iure consequatur?</p>
+<h3>contatto 3</h3>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos eveniet atque, veniam ex porro corrupti amet pariatur expedita. Dignissimos pariatur suscipit accusamus ea expedita officia quia deserunt magnam aspernatur, illum provident quibusdam tempora, autem commodi quas repellendus eveniet iure consequatur?</p>
+</body>
+</html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="about">About</a></li>
-            <li><a href="#">link</a></li>
+            <li><a href="contacts">Contacts</a></li>
         </ul>
     </nav>
 </header>
